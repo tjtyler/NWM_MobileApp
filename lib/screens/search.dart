2970ad8e9cbcec79772cf602c985a817047dc288
tjtyler <travis.tyler8957@gmail.com>
2022-11-01 21:49:33 -0600

    things are working except for the location search

--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_google_places_hoc081098/flutter_google_places_hoc081098.dart';
+import 'package:google_api_headers/google_api_headers.dart';
+import 'package:google_maps_webservice/places.dart';
 
+// https://www.youtube.com/watch?v=tfFByL7F-00&t=238s
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
 
@@ -10,28 +14,79 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
-  final _backArrow = const Icon(Icons.arrow_back);
-  Widget _appBarTitle = const Text('River Forecast');
+  String googleApikey = "AIzaSyAZbG9wTZbKpju3qk-rid9JlusNkfO2L2M";
+  GoogleMapController? mapController; //contrller for Google map
+  final TextEditingController _searchController = TextEditingController();
+  CameraPosition? cameraPosition;
+  LatLng startLocation = const LatLng(27.6602292, 85.308027);
+  String location = "Search Location";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: _buildBar(context),
-        body: // Container(child: _googleMap(_center)),
-            Text("Search Screen"));
-  }
+        appBar: AppBar(
+          title: const Text("Location Search"),
+          backgroundColor: const Color.fromARGB(255, 133, 85, 136),
+        ),
+        body: Column(children: [
+          //search autoconplete input
+          Positioned(
+              //search input bar
+              top: 10,
+              child: InkWell(
+                  onTap: () async {
+                    var place = await PlacesAutocomplete.show(
+                        context: context,
+                        apiKey: googleApikey,
+                        mode: Mode.overlay,
+                        types: [],
+                        strictbounds: false,
+                        components: [Component(Component.country, 'np')],
+                        //google_map_webservice package
+                        onError: (err) {
+                          print(err);
+                        });
 
-  PreferredSizeWidget _buildBar(BuildContext context) {
-    return AppBar(
-      centerTitle: true,
-      title: _appBarTitle,
-      leading: IconButton(
-        icon: _backArrow,
-        onPressed: () {
-          Navigator.pop(context);
-        },
-      ),
-      backgroundColor: const Color.fromARGB(255, 133, 85, 136),
-    );
+                    if (place != null) {
+                      setState(() {
+                        location = place.description.toString();
+                      });
+
+                      //form google_maps_webservice package
+                      final plist = GoogleMapsPlaces(
+                        apiKey: googleApikey,
+                        apiHeaders: await const GoogleApiHeaders().getHeaders(),
+                        //from google_api_headers package
+                      );
+                      String placeid = place.placeId ?? "0";
+                      final detail = await plist.getDetailsByPlaceId(placeid);
+                      final geometry = detail.result.geometry!;
+                      final lat = geometry.location.lat;
+                      final lang = geometry.location.lng;
+                      var newlatlang = LatLng(lat, lang);
+
+                      //move map camera to selected place with animation
+                      mapController?.animateCamera(
+                          CameraUpdate.newCameraPosition(
+                              CameraPosition(target: newlatlang, zoom: 17)));
+                    }
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Card(
+                      child: Container(
+                          padding: const EdgeInsets.all(0),
+                          width: MediaQuery.of(context).size.width - 40,
+                          child: ListTile(
+                            title: Text(
+                              location,
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                            trailing: const Icon(Icons.search),
+                            dense: true,
+                          )),
+                    ),
+                  )))
+        ]));
   }
 }
