5528136d1d4ebb0ddd618ac7071899756012bf60
tjtyler <travis.tyler8957@gmail.com>
2022-11-16 11:20:10 -0700

    have some components for favorites UI

--- lib/screens/favorites.dart
+++ lib/screens/favorites.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
 // https://github.com/cmailoa/google_places_autocomplete/blob/master/main.dart
 // https://medium.com/@ch.a.andremailoa/your-own-flutter-google-places-autocomplete-3e60c0358280
@@ -24,16 +25,55 @@ class _FavoritesScreenState extends State<FavoritesScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      centerTitle: true,
-      title: _appBarTitle,
-      leading: IconButton(
-        icon: _backArrow,
-        onPressed: () {
-          Navigator.pop(context);
-        },
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      appBar: AppBar(
+        centerTitle: true,
+        title: _appBarTitle,
+        leading: IconButton(
+          icon: _backArrow,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        backgroundColor: const Color.fromARGB(255, 133, 85, 136),
       ),
-      backgroundColor: const Color.fromARGB(255, 133, 85, 136),
-    ));
+      body: Center(
+          child: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(30),
+            child: Text('<River Name>',
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 30)),
+          ),
+          Stack(children: [
+            Container(
+                alignment: Alignment.center,
+                child: Image.asset(
+                    "/Users/travistyler/development/nwm_river_forecast/lib/resources/images/water.jpeg",
+                    width: 200,
+                    height: 200)),
+            Container(
+              padding: const EdgeInsets.all(20),
+              alignment: Alignment.topCenter,
+              child: Column(children: const [
+                Text('Max: ',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 18)),
+                Text('Mean: ',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 18))
+              ]),
+            ),
+          ])
+        ],
+      )),
+    );
   }
 }
